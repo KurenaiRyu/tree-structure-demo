2e46474ffab4b4e7c80185566f33ab9a2600cc08
Kurenai <kurenai233@yahoo.com>
2019-04-05 07:05:08 +0800

    fix(): 注释有问题代码

--- src/main/java/org/kurenai/treestructure/binarytree/TreeInsert.java
+++ src/main/java/org/kurenai/treestructure/binarytree/TreeInsert.java
@@ -9,7 +9,13 @@ package org.kurenai.treestructure.binarytree;
 
 public class TreeInsert {
 
+    /**
+     * 递归有序插入
+     * @param root 树的根节点
+     * @param val 插入的数值
+     */
     public static void recursionOrderedInsert(TreeNode root, int val) {
+        //
         if (val < root.val) {
             if (root.left == null) {
                 root.left = new TreeNode(val);
@@ -25,14 +31,22 @@ public class TreeInsert {
         }
     }
 
+    /**
+     * 递归无序插入
+     * @param root 树的根节点
+     * @param val 插入的数值
+     */
     public static void recursionDisorderInsert(TreeNode root, int val) {
-            if (root.left == null) {
-                root.left = new TreeNode(val);
-            } else if (root.right == null){
-                root.right = new TreeNode(val);
-            } else {
-                recursionOrderedInsert(root.left, val);
-                recursionOrderedInsert(root.right, val);
-            }
+        /**
+         * 有错误
+         */
+//            if (root.left == null) {
+//                root.left = new TreeNode(val);
+//            } else if (root.right == null){
+//                root.right = new TreeNode(val);
+//            } else {
+//                recursionOrderedInsert(root.left, val);
+//                recursionOrderedInsert(root.right, val);
+//            }
     }
 }
